332e93d3e78b637182162255608e88f1fccfe638
Mubin Ansari <mubinansari910@gmail.com>
2022-12-09 09:21:40 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Guess My Number Game Starter Code
+
+A simple game built using HTML, CSS & Vanilla JS where you guess a random number that computer has selected.
+
+Built for CSI-PCE's content starter in Pillai College Education Fair 2022.
---
+++ script.js
@@ -0,0 +1,12 @@
+const msg1 = document.getElementById('message1');
+const msg2 = document.getElementById('message2');
+const msg3 = document.getElementById('message3');
+const button = document.getElementById('my_btn');
+
+const answer = Math.floor(Math.random() * 100) + 1;
+let no_of_guesses = 0;
+const guessed_nums = [];
+
+function play() {
+  
+}
